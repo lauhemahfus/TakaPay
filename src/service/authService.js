29b4abef2a682509e61b31a737e36e84ca9b2049
lauhemahfus <lauhemahfus@gmail.com>
2026-01-26 22:24:57 +0600

    add password hashing before stroing in database

--- src/service/authService.js
+++ src/service/authService.js
@@ -1,13 +1,17 @@
 import { db } from "../config/prisma.js";
+import { hashPassword } from "../utils/password.js";
 
 export async function createUser (userInfo) {
     try {
+        const hashedPassword = await hashPassword(userInfo.password);
+        console.log(hashedPassword);
+        
         return db.user.create({
             data:{
                 name: userInfo.name,
                 email: userInfo.email,
                 phone: userInfo.phone,
-                password: userInfo.password,
+                password: hashedPassword,
             }
         });
 
